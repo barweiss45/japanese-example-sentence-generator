e81eaaa27cab1116cea46f6978476d6a1cbbc7ca
Barry We1iss <barweiss@cisco.com>
2023-05-19 08:43:19 -0400

    Testing bot.py scripti

--- bot.py
+++ bot.py
@@ -1,24 +1,35 @@
 import os
 import discord
+from langchain.llms import OpenAI
+from discord.ext import commands
+from forvo_api import get_pronounciation
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('.env')
 
 TOKEN = os.getenv('discord_api_key')
+openai_api_key = os.getenv('openai_api_key')
 
+llm = OpenAI(model_name="text-davinci-003", openai_api_key=openai_api_key)
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
-        
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(intents=intents, command_prefix='!')
 
+@bot.event    
+async def on_ready():
+    guild = discord.utils.get(bot.guilds)
+    print(f'{bot.user} has connected to Discord!')
+    print(f'{bot.user} is active in {guild.id}')
+    
+@bot.command(name='ping')
+async def on_message(ctx):
+    await ctx.send('pong')
+    
+@bot.command(name='llm')
+async def on_message(ctx, arg):
+    llm_output = llm(arg)
+    print(f"{ctx.author}Sent: {ctx.message}\n\nLLM Sent: {llm_output}")
+    await ctx.send(llm_output)
 
-client = MyClient()
-client.run('TOKEN')
+bot.run(TOKEN)
